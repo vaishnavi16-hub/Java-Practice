eb8861eaf569234f4da81b9369f479ef3c9575f5
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-06-12 18:34:24 +0530

    Add files via upload

---
+++ CONSTRUCTOR_PRACTICE/src/Employee.java
@@ -0,0 +1,12 @@
+
+public class Employee {
+
+	
+	public Employee(String Name, int id, float salary) {
+		
+		System.out.println("<--------------Employee information---------->");
+		System.out.println("Name = "+ Name);
+		System.out.println("Id  = "+ id);
+		System.out.println("salary = "+ salary);
+	}
+}
---
+++ CONSTRUCTOR_PRACTICE/src/employeeMain.java
@@ -0,0 +1,7 @@
+
+public class employeeMain {
+	public static void main(String[]args) {
+		
+		Employee e1 = new Employee(" vaishnavi",40,60009.00f);		
+	}
+}
