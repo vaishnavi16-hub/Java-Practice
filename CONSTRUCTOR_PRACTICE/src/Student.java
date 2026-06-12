53fe7e74a994b3cd2ac125c0b72035a2d07af700
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-06-12 15:53:37 +0530

    Add files via upload

---
+++ CONSTRUCTOR_PRACTICE/src/Student.java
@@ -0,0 +1,12 @@
+
+public class Student {
+
+	public Student(String name, int rollNo, String course) {
+		System.out.println("Student Information------->");
+		System.out.println("Name ---"+name);
+		System.out.println("RollNo ---"+rollNo);
+		System.out.println("Course----"+course);
+		
+	}
+	
+}
---
+++ CONSTRUCTOR_PRACTICE/src/studentMain.java
@@ -0,0 +1,8 @@
+
+public class studentMain {
+	public static void main(String[]args) {
+		
+		
+		Student S1 = new Student("vaishnavi", 23, "Engineering");		
+	}
+}
